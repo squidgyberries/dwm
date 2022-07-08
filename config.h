73300f003ef955362281f8a3b145ec5c64ef30f9
squidgyberries <squidgyberriesgit@protonmail.com>
2022-07-08 12:46:49 +0800

    nord theme

--- config.h
+++ config.h
@@ -6,17 +6,15 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "JetBrains Mono:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+#include "themes/nord.h"
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+  /*                     fg       bg      border */
+  [SchemeNorm]       = { gray3,   black,  gray2 },
+  [SchemeSel]        = { gray4,   blue,   blue  },
 };
 
 /* tagging */
