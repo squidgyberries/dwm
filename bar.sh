b8218417eb94db1c7390c4b68d6c53c513819f45
squidgyberries <squidgyberriesgit@protonmail.com>
2022-07-24 16:55:42 +0800

    switch to fuzzytime (squidgyberries/fuzzytime)

--- bar.sh
+++ bar.sh
@@ -4,8 +4,9 @@ while true
 do
   BAT1=`cat /sys/class/power_supply/BAT0/status`
   BAT2=`cat /sys/class/power_supply/BAT0/capacity`"%"
-  TIME=`date +"%a, %b %d, %H:%M"`
-  xsetroot -name " ${BAT1} ${BAT2} | ${TIME} "
+  TIME1=`date +"%a, %b %d"`
+  TIME2=`fuzzytime`
+  xsetroot -name " ${BAT1} ${BAT2} | ${TIME1}, ${TIME2} "
   
   sleep 2
 done
